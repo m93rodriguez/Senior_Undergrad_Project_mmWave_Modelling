aefbef4ac16e7bc599e97e3479f928e898750365
m93rodriguez <mrodri39@eafit.edu.co>
2016-10-04 17:14:20 -0500

    Added main function and Modified CIR generating Functions

--- src/functions/Generate_MIMO_CIR.m
+++ src/functions/Generate_MIMO_CIR.m
@@ -1,8 +1,10 @@
-function H=Generate_MIMO_CIR(System,CovarianceMatrix,MultipathPower,MultipathPhase)
+function MIMO_CIR=Generate_MIMO_CIR(System,CovarianceMatrix,MultipathPower,MultipathPhase,...
+    TimePositions,SpatialLobes)
 %Calculate Multiple-Input Multiple-Output system channel impulse response.
-% H=Generate_MIMO_CIR(System,CovarianceMatrix,MultipathPower,MultipathPhase);
+% MIMO_CIR=Generate_MIMO_CIR(System,CovarianceMatrix,MultipathPower,MultipathPhase);
 
 H=cell(1,length(MultipathPower.Amplitude));
+PDP=H;
 for i=1:length(MultipathPower.Amplitude)
     Rayleigh_iid_Matrix=randn(System.Nr,System.Nt)+1i*randn(System.Nr,System.Nt);
     [Vector_r,Value_r]=eig(CovarianceMatrix.Receive{i});
@@ -10,7 +12,26 @@ for i=1:length(MultipathPower.Amplitude)
     
     H{i}=Vector_r*sqrt(Value_r)*Rayleigh_iid_Matrix*sqrt(Value_t)*Vector_t';
     H{i}=MultipathPower.Amplitude(i)*exp(1i*MultipathPhase.Phase(i))*H{i};
-    
+    PDP{i}=H{i}.*conj(H{i}); 
 end
+MIMO_CIR.Index=TimePositions.TimeIndex;
+MIMO_CIR.H=H;
+MIMO_CIR.Time=TimePositions.ExcessTime;
+MIMO_CIR.AOA=SpatialLobes.AOA;
+MIMO_CIR.AOD=SpatialLobes.AOD;
+MIMO_CIR.PDP=PDP;
+
+h=cell(System.Nr,System.Nt);
+n=1:TimePositions.TimeIndex(end);
+for i=1:System.Nr
+    for j=1:System.Nt
+        h{i,j}=zeros(size(n));
+        for cont=1:length(TimePositions.TimeIndex)
+            h{i,j}(cont)=H{cont}(i,j);
+        end
+    end
+end
+
+MIMO_CIR.h=h;
 
 end
--- src/functions/Generate_SISO_CIR.m
+++ src/functions/Generate_SISO_CIR.m
@@ -1,7 +1,26 @@
-function h=Generate_SISO_CIR(TimePositions,MultipathPower,MultipathPhase)
+function SISO_CIR=Generate_SISO_CIR(System,TimePositions,MultipathPower,MultipathPhase,SpatialLobes)
 %Generate Single-Input Single-Output Channel Impulse Response
 % h=Generate_SISO_CIR(TimePositions,MultipathPower,MultipathPhase)
 
+SISO_CIR=struct;
+
+n=1:TimePositions.TimeIndex(end);
+
 h=zeros(1,TimePositions.TimeIndex(end));
 h(TimePositions.TimeIndex)=MultipathPower.Power.*exp(1i*MultipathPhase.Phase);
+
+Time=(n-1)*System.TimeResolution;
+
+AOD=zeros(size(n));
+AOA=AOD;
+AOD(TimePositions.TimeIndex)=SpatialLobes.AOD;
+AOA(TimePositions.TimeIndex)=SpatialLobes.AOA;
+
+SISO_CIR.Index=TimePositions.TimeIndex;
+SISO_CIR.h=h;
+SISO_CIR.Time=Time;
+SISO_CIR.AOD=AOD;
+SISO_CIR.AOA=AOA;
+SISO_CIR.PDP=h.*conj(h);
+
 end
